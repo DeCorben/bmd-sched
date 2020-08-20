4d41b56331764c54d389faf304d7fe15e1ebdf17
DeCorben <32888104+DeCorben@users.noreply.github.com>
2020-08-20 14:51:33 -0400

    meaningless test

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import Appt from 'bmd-appt'
 export default class Sched extends React.Component{
     constructor(props){
         super(props)
-        //this.data = props.data
+        //this.data = props.data plus meaningless change
         this.state = {
             heading: props.data.heading,
             schedule: this.parse()
